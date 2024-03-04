807d3958a7b8fab9969634dc2c8a48d2b7ca87d9
JonhannesNilsson <jonte9203@hotmail.com>
2024-03-04 10:31:02 +0100

    InitialCommit

---
+++ VVCyberAware/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace VVCyberAware.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
