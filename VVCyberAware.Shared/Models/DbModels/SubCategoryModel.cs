3d51d701c4ebd7bfdf777af6df0b43f3d292c8e6
PaulineI <pauline.ibraim@gmail.com>
2024-03-04 11:18:34 +0100

    DbModels

---
+++ VVCyberAware.Shared/Models/DbModels/SubCategoryModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVCyberAware.Shared.Models.DbModels
+{
+	public class SubCategoryModel
+	{
+		[Key]
+		public int Id { get; set; }
+
+		public string Description { get; set; } = null!;
+
+		public List<QuestionModel>? Questions { get; set; }
+	}
+}
