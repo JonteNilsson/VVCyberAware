a16077afc34d454474a8deab5325aad9c228c123
PaulineI <pauline.ibraim@gmail.com>
2024-03-06 10:10:55 +0100

    ApiModels

---
+++ VVCyberAware.Shared/Models/ApiModels/QuestionApiModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVCyberAware.Shared.Models.ApiModels
+{
+	public class QuestionApiModel
+	{
+		public int Id { get; set; }
+
+		public string QuestionText { get; set; } = null!;
+
+		public Dictionary<string, bool> Answers { get; set; } = new();
+
+		public string? Explanation { get; set; }
+
+		public SubCategoryModel? SubCategory { get; set; }
+
+		public int SubCategoryId { get; set; }
+	}
+}
---
+++ VVCyberAware.Shared/Models/ApiModels/SubCategoryApiModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VVCyberAware.Shared.Models.ApiModels
+{
+	public class SubCategoryModel
+	{
+		public int Id { get; set; }
+
+		public string Description { get; set; } = null!;
+
+		public SegmentModel? Segment { get; set; } // Nav prop
+		public int SegmentId { get; set; }
+
+	}
+}
