8ffb55f92a999c4ea567c22f10f097679fd89915
PaulineI <pauline.ibraim@gmail.com>
2024-03-06 10:02:07 +0100

    UserochAdminUser Default

---
+++ VVCyberAware.Shared/Models/ApiModels/CategoryApiModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVCyberAware.Shared.Models.DbModels;
+
+namespace VVCyberAware.Shared.Models.ApiModels
+{
+	public class CategoryApiModel
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = null!;
+
+		public string Description { get; set; } = null!;
+
+		public List<SegmentModel>? Segments { get; set; }
+
+	}
+}
