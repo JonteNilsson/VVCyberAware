9de80cf39700d813e8f723c1d9035d034a8c8ee2
PaulineI <pauline.ibraim@gmail.com>
2024-04-09 14:13:04 +0200

    UnitTest

---
+++ DBTests/UnitTestCalculation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VVCyberAware.Methods;
+
+namespace DBTests
+{
+    public class UnitTestCalculation
+    {
+        [Fact]
+        public void TestCalculation()
+        {
+            // Arrange
+            CalculatingProcent calculatingProcent = new CalculatingProcent();
+
+            //Act
+
+            double testsum = calculatingProcent.Calculation(2, 2);
+
+            //Assert
+            Assert.Equal(100, testsum);
+        }
+
+    }
+}
---
+++ VVCyberAware/Methods/CalculatingProcent.cs
@@ -0,0 +1,18 @@
+﻿namespace VVCyberAware.Methods
+{
+    public class CalculatingProcent
+    {
+        public int Calculation(int y, int x)
+        {
+            if (x != 0)
+            {
+                return (int)Math.Floor((y / (double)x) * 100);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+
+}
